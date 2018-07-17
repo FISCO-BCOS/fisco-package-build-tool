401c846f8db52168ca98bfd61ef816d2b88047b5
Octopuswang <912554887@qq.com>
2018-07-17 20:37:00 +0800

    添加操作系统检查的流程

--- installation_dependencies/dependencies/scripts/os_version_check.sh
+++ installation_dependencies/dependencies/scripts/os_version_check.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 set -e
 
@@ -15,226 +15,119 @@ case $(uname -s) in
 Darwin)
     case $(sw_vers -productVersion | awk -F . '{print $1"."$2}') in
         10.9)
-            echo "Installing $myname dependencies on OS X 10.9 Mavericks."
-            ;;
-        10.10)
-            echo "Installing $myname dependencies on OS X 10.10 Yosemite."
-            ;;
-        10.11)
-            echo "Installing $myname dependencies on OS X 10.11 El Capitan."
-            ;;
-        10.12)
-            echo "Installing $myname dependencies on macOS 10.12 Sierra."
-            echo ""
-            echo "NOTE - You are in unknown territory with this preview OS."
-            echo "Even Homebrew doesn't have official support yet, and there are"
-            echo "known issues (see https://github.com/ethereum/webthree-umbrella/issues/614)."
-            echo "If you would like to partner with us to work through these issues, that"
-            echo "would be fantastic.  Please just comment on that issue.  Thanks!"
+            echo "Running $myname on OS X 10.9 Mavericks."
             ;;
         *)
             echo "Unsupported macOS version."
-            echo "We only support Mavericks, Yosemite and El Capitan, with work-in-progress on Sierra."
             exit 1
             ;;
-    esac
+    esac #case $(sw_vers
 
-    # Check for Homebrew install and abort if it is not installed.
-    brew -v > /dev/null 2>&1 || { echo >&2 "ERROR - cpp-ethereum requires a Homebrew install.  See http://brew.sh."; exit 1; }
-
-    # And finally install all the external dependencies.
-    brew install \
-        leveldb \
-        libmicrohttpd \
-        miniupnpc
-
-    ;;
-
-#------------------------------------------------------------------------------
-# FreeBSD
-#------------------------------------------------------------------------------
-FreeBSD)
-    echo "Installing $myname dependencies on FreeBSD."
-    echo "ERROR - $myname doesn't have FreeBSD support yet."
-    exit 1
-    ;;
-
+    ;; #Darwin)
+    
 #------------------------------------------------------------------------------
 # Linux
 #------------------------------------------------------------------------------
 Linux)
 
-    # Detect if sudo is needed.
-    if [ $(id -u) != 0 ]; then
-        SUDO="sudo"
+    if [ ! -f "/etc/os-release" ];then
+        { echo >&2 "ERROR - Unsupported or unidentified Linux distro."; exit 1; }
     fi
 
+    DISTRO_NAME=$(. /etc/os-release; echo $NAME)
+    echo "Linux distribution: $DISTRO_NAME."
+
+    case $DISTRO_NAME in
 #------------------------------------------------------------------------------
-# Arch Linux
+# Ubuntu  # At least 16.04
 #------------------------------------------------------------------------------
+        Ubuntu*)
 
-    if [ -f "/etc/arch-release" ]; then
+            echo "Running $myname on Ubuntu."
 
-        echo "Installing $myname dependencies on Arch Linux."
-
-    elif [ -f "/etc/os-release" ]; then
-
-        DISTRO_NAME=$(. /etc/os-release; echo $NAME)
-	echo "Linux distribution: $DISTRO_NAME."
-        case $DISTRO_NAME in
-
-        Debian*)
-            echo "Installing cpp-ethereum dependencies on Debian Linux."
-
-            $SUDO apt-get -q update
-            $SUDO apt-get -qy install \
-                build-essential \
-                libboost-all-dev \
-                libcurl4-openssl-dev \
-                libgmp-dev \
-                libleveldb-dev \
-                libmicrohttpd-dev \
-                libminiupnpc-dev
-            ;;
-
-        Fedora)
-            echo "Installing cpp-ethereum dependencies on Fedora Linux."
-            $SUDO dnf -qy install \
-                gcc-c++ \
-                boost-devel \
-                leveldb-devel \
-                curl-devel \
-                libmicrohttpd-devel \
-                gmp-devel
-            ;;
-
-#------------------------------------------------------------------------------
-# Ubuntu
-#
-# TODO - I wonder whether all of the Ubuntu-variants need some special
-# treatment?
-#
-# TODO - We should also test this code on Ubuntu Server, Ubuntu Snappy Core
-# and Ubuntu Phone.
-#
-# TODO - Our Ubuntu build is only working for amd64 and i386 processors.
-# It would be good to add armel, armhf and arm64.
-# See https://github.com/ethereum/webthree-umbrella/issues/228.
-#------------------------------------------------------------------------------
-        Ubuntu|LinuxMint)
-            echo "Installing cpp-ethereum dependencies on Ubuntu."
-            if [ "$TRAVIS" ]; then
-                # Setup prebuilt LLVM on Travis CI:
-                $SUDO apt-get -qy remove llvm  # Remove confilicting package.
-                echo "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main" | \
-                    $SUDO tee -a /etc/apt/sources.list > /dev/null
-                LLVM_PACKAGES="llvm-3.9-dev libz-dev"
+            UBUNTU_VERSION=""
+            type lsb_release >/dev/null 2>&1
+            if [ $? -eq 0 ];then
+                UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}')
+            else
+                UBUNTU_VERSION=$(. /etc/os-release; echo $VERSION | awk '{print $1}')
             fi
-            $SUDO apt-get -q update
-            $SUDO apt-get install -qy --no-install-recommends --allow-unauthenticated \
-                build-essential \
-                libboost-all-dev \
-                libcurl4-openssl-dev \
-                libgmp-dev \
-                libleveldb-dev \
-                libmicrohttpd-dev \
-                libminiupnpc-dev \
-                $LLVM_PACKAGES
-            ;;
 
+            echo "Ubuntu Version => $UBUNTU_VERSION"
+
+            ver=$(echo "$UBUNTU_VERSION" | awk -F . '{print $1$2}')
+            #Ubuntu 16.04 or Ubuntu 16.04+
+            if [ $ver -lt 1604 ];then
+                { echo >&2 "ERROR - Unsupported Ubuntu Version. At least 16.04 is required."; exit 1; }
+            fi
+
+            ;;
+#------------------------------------------------------------------------------
+# CentOS  # At least 7.2
+#------------------------------------------------------------------------------
         CentOS*)
-            echo "Installing cpp-ethereum dependencies on CentOS."
-            # Enable EPEL repo that contains leveldb-devel
-            $SUDO yum -y -q install epel-release
-            $SUDO yum -y -q install \
-                make \
-                gcc-c++ \
-                boost-devel \
-                leveldb-devel \
-                curl-devel \
-                libmicrohttpd-devel \
-                gmp-devel \
-                openssl openssl-devel
+            echo "Running $myname on CentOS."
+            CENTOS_VERSION=""
+            if [ -f /etc/centos-release ];then
+                CENTOS_VERSION=$(cat /etc/centos-release)
+            elif [ -f /etc/redhat-release ];then
+                CENTOS_VERSION=$(cat /etc/redhat-release)
+            elif [ -f /etc/system-release ];then
+                CENTOS_VERSION=$(cat /etc/system-release)
+            fi
+
+            if [ -z "$CENTOS_VERSION" ];then
+                { echo >&2 "unable to determine CentOS Version."; exit 1; }
+            fi
+
+            echo "CentOS Version => $CENTOS_VERSION"
+            ver=$(echo "$CENTOS_VERSION" | awk '{print $4}' | awk -F . '{print $1$2}')
+
+            #CentOS 7.2 or CentOS 7.2+
+            if [ $ver -lt 72 ];then
+                { echo >&2 "ERROR - Unsupported CentOS Version. At least 7.2 is required."; exit 1; }
+            fi
             ;;
-		#add Oracle Linux Server dependencies
-		Oracle*)
-            echo "Installing cpp-ethereum dependencies on Oracle Linux Server."
-            # Enable EPEL repo that contains leveldb-devel
-            $SUDO yum -y -q install epel-release
-            $SUDO yum -y -q install \
-                make \
-                gcc-c++ \
-                boost-devel \
-                leveldb-devel \
-                curl-devel \
-                libmicrohttpd-devel \
-                gmp-devel \
-                openssl openssl-devel
-            ;;
-
-        *)
-            echo "Unsupported Linux distribution: $DISTRO_NAME."
-            exit 1
-            ;;
-
-        esac
-
-    elif [ -f "/etc/alpine-release" ]; then
-
-        # Alpine Linux
-        echo "Installing cpp-ethereum dependencies on Alpine Linux."
-        $SUDO apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/ \
-            g++ \
-            make \
-            boost-dev \
-            curl-dev \
-            libmicrohttpd-dev \
-            leveldb-dev
-
-    else
-
-        case $(lsb_release -is) in
-
 #------------------------------------------------------------------------------
-# OpenSUSE
+# Oracle Linux Server # At least 7.4
 #------------------------------------------------------------------------------
-        openSUSE*)
-            #openSUSE
-            echo "Installing cpp-ethereum dependencies on openSUSE."
-            echo "ERROR - 'install_deps.sh' doesn't have openSUSE support yet."
-            echo "See http://cpp-ethereum.org/building-from-source/linux.html for manual instructions."
-            echo "If you would like to get 'install_deps.sh' working for openSUSE, that would be fantastic."
-            echo "See https://github.com/ethereum/webthree-umbrella/issues/552."
-            exit 1
-            ;;
+        Oracle*) 
+            echo "Running $myname on Oracle Linux."
+            ORACLE_LINUX_VERSION=""
+            if [ -f /etc/oracle-release ];then
+                ORACLE_LINUX_VERSION=$(cat /etc/oracle-release)
+            elif [ -f /etc/system-release ];then
+                ORACLE_LINUX_VERSION=$(cat /etc/system-release)
+            fi
 
+            if [ -z "$ORACLE_LINUX_VERSION" ];then
+                { echo >&2 "unable to determine Oracle Linux version."; exit 1; }
+            fi
+
+            echo "Oracle Linux Version => $ORACLE_LINUX_VERSION"
+            ver=$(echo "$ORACLE_LINUX_VERSION" | awk '{print $5}' | awk -F . '{print $1$2}')
+
+            #Oracle Linux 7.4 or Oracle Linux 7.4+
+            if [ $ver -lt 74 ];then
+                { echo >&2 "ERROR - Unsupported Oracle Linux, At least 7.4 Oracle Linux is required."; exit 1; }
+            fi
+
+            ;;
 #------------------------------------------------------------------------------
-# Other (unknown) Linux
-# Major and medium distros which we are missing would include Mint, CentOS,
-# RHEL, Raspbian, Cygwin, OpenWrt, gNewSense, Trisquel and SteamOS.
+# Other Linux
 #------------------------------------------------------------------------------
         *)
-            #other Linux
-            echo "ERROR - Unsupported or unidentified Linux distro."
-            echo "See http://cpp-ethereum.org/building-from-source/linux.html for manual instructions."
-            echo "If you would like to get your distro working, that would be fantastic."
-            echo "Drop us a message at https://gitter.im/ethereum/cpp-ethereum-development."
-            exit 1
+            { echo >&2 "ERROR - Unsupported Linux distribution: $DISTRO_NAME."; exit 1; }
             ;;
-        esac
-    fi
-    ;;
+    esac # case $DISTRO_NAME
+
+    ;; #Linux)
 
 #------------------------------------------------------------------------------
 # Other platform (not Linux, FreeBSD or macOS).
-# Not sure what might end up here?
-# Maybe OpenBSD, NetBSD, AIX, Solaris, HP-UX?
 #------------------------------------------------------------------------------
 *)
     #other
-    echo "ERROR - Unsupported or unidentified operating system."
-    echo "See http://cpp-ethereum.org/building-from-source/ for manual instructions."
-    echo "If you would like to get your operating system working, that would be fantastic."
-    echo "Drop us a message at https://gitter.im/ethereum/cpp-ethereum-development."
+    { echo >&2 "ERROR - Unsupported or unidentified operating system."; exit 1; }
     ;;
 esac
